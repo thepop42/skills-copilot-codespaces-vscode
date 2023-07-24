9a9f2ea0b50a88be6024fc083b61e31f5fd55256
Pop <45603971+thepop42@users.noreply.github.com>
2023-07-24 13:49:07 +0000

    update

--- comments.js
+++ comments.js
@@ -1,14 +1,4 @@
 // create web server with express
-import express from 'express';
-const app = express();
-const port = 3000;
-
-// create a route with express
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-// start the server
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+function calculateNumbers(var1, var2) {
+    return var1 + var2;
+}
